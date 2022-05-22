5a5be55f53d80295bca1f5c96276e2855d89addf
Yassine EL GHALI <yelghali@gmail.com>
2022-05-22 15:01:16 +0200

    Update hasura-api-withoutauth.tf

--- hasura-api-withoutauth.tf
+++ hasura-api-withoutauth.tf
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "my_service_plan" {
 
 
 resource "azurerm_app_service" "hasura_app_service_container" {
- name                    = "elhasuragraphqlserver"
+ name                        = "${random_string.myprefix.result}apphasura"
  location                = "France central"
  resource_group_name = azurerm_resource_group.terrazuragrp.name
  app_service_plan_id     = azurerm_app_service_plan.my_service_plan.id
